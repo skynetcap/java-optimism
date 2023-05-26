f62c1584fd7ffc1e415bf09484a76d3c4f45b28d
skynetcap <100323448+skynetcap@users.noreply.github.com>
2023-05-26 08:08:40 -0700

    get contract bytecode in demo

--- src/main/java/OptimismMain.java
+++ src/main/java/OptimismMain.java
@@ -10,10 +10,11 @@ import java.io.IOException;
 import java.util.List;
 
 public class OptimismMain {
-    public static void main(String[] args) throws IOException {
-        Web3j web3 = Web3j.build(new HttpService("https://mainnet.optimism.io"));
-        Logger LOGGER = LoggerFactory.getLogger(OptimismMain.class);
 
+    private static final Web3j web3 = Web3j.build(new HttpService("https://mainnet.optimism.io"));
+    private static final Logger LOGGER = LoggerFactory.getLogger(OptimismMain.class);
+
+    public static void main(String[] args) throws IOException {
         // token
         LOGGER.info("ChainID: " + web3.ethChainId().send().getChainId().toString());
         LOGGER.info("0x715a Balance: " +
@@ -27,13 +28,34 @@ public class OptimismMain {
                         .send()
                         .getBlock()
                         .getTransactions();
-        
+
         txs.forEach(tx -> {
             EthBlock.TransactionObject transaction = (EthBlock.TransactionObject) tx.get();
             LOGGER.info("TX: " + transaction.getHash());
             LOGGER.info("From: " + transaction.getFrom());
             LOGGER.info("To: " + transaction.getTo());
             LOGGER.info("Raw: " + transaction.getInput());
+
+            try {
+                String code = web3.ethGetCode(transaction.getTo(),
+                        DefaultBlockParameter.valueOf("latest")).send().getCode();
+                LOGGER.info("'To' Contract bytecode: " + code);
+                decompileBytecode(code);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         });
+    }
+
+    public static void decompileBytecode(String code) {
+        String inputCode = code.substring(2);
+        for (int i = 0; i < inputCode.length(); i+=2) {
+            // read 2 chars as a switch
+            switch (inputCode.charAt(i) + inputCode.charAt(i + 1)) {
+                case 0x00 -> LOGGER.info("null");
+                case 0x36 -> LOGGER.info("CALLDATASIZE");
+                default -> LOGGER.info("???: " + (byte) (inputCode.charAt(i) + inputCode.charAt(i + 1)));
+            }
+        }
     }
 }
